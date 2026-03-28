10fa7657715250e7f8c77af3ad98612ad3705010
tambabarquoi2000-crypto <tambabarquoi2000@gmail.com>
2026-03-28 10:43:40 -1200

    javascript filter lesson enhancement

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,165 @@
+today = new Date();
+
+const currentYear = document.querySelector("#current-year")
+currentYear.textContent = today.getFullYear();
+
+const lastVisited = document.querySelector("#last-modified");
+lastVisited.innerHTML = `<span class="highlight"> Last Modified ${document.lastModified}`
+
+const hamburger = document.querySelector("#menu");
+const navigator = document.querySelector(".navigation")
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("open")
+    navigator.classList.toggle("open")
+})
+
+const temples = [
+    {
+        templeName: "Aba Nigeria",
+        location: "Aba, Nigeria",
+        dedicated: "2005, August, 7",
+        area: 11500,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    },
+    {
+        templeName: "Manti Utah",
+        location: "Manti, Utah, United States",
+        dedicated: "1888, May, 21",
+        area: 74792,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    },
+    {
+        templeName: "Payson Utah",
+        location: "Payson, Utah, United States",
+        dedicated: "2015, June, 7",
+        area: 96630,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    },
+    {
+        templeName: "Yigo Guam",
+        location: "Yigo, Guam",
+        dedicated: "2020, May, 2",
+        area: 6861,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    },
+    {
+        templeName: "Washington D.C.",
+        location: "Kensington, Maryland, United States",
+        dedicated: "1974, November, 19",
+        area: 156558,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    },
+    {
+        templeName: "Lima Perú",
+        location: "Lima, Perú",
+        dedicated: "1986, January, 10",
+        area: 9600,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    },
+    {
+        templeName: "Mexico City Mexico",
+        location: "Mexico City, Mexico",
+        dedicated: "1983, December, 2",
+        area: 116642,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    },
+    // Add more temple objects here...
+    {
+        templeName: "Modesto California",
+        location: "Modesto, California",
+        dedicated: "2023, October, 17",
+        area: 30000,
+        imageUrl:
+            "https://churchofjesuschristtemples.org/assets/img/temples/modesto-california-temple/modesto-california-temple-69614-thumb.jpg"
+    },
+    {
+        templeName: "Monrovia Liberia",
+        location: "Monrovia, Liberia",
+        dedicated: "Still in Construction",
+        area: "6000",
+        imageUrl:
+            "https://churchofjesuschristtemples.org/assets/img/temples/monrovia-liberia-temple/monrovia-liberia-temple-22347.jpg"
+    },
+    {
+        templeName: "Brigham City Utah",
+        location: "Brigham City, Utah",
+        dedicated: "2010, July, 31",
+        area: 36000,
+        imageUrl:
+            "https://churchofjesuschristtemples.org/assets/img/temples/brigham-city-utah-temple/brigham-city-utah-temple-3907-thumb.jpg"
+    }
+];
+
+const currentDisplay = document.querySelector("#temple-cards");
+
+
+function displayItems (temples){
+    let templeCards = "";
+    for (const temple of temples) {
+    const templeName = temple.templeName.split()[0];
+    const templeLocation = temple.location.split(",")[0];
+    templeCards += `<div class=${templeName}>
+        <h2>${temple.templeName}</h2>
+        <p>Location: ${temple.location}</p>
+        <p>Dedicated: ${temple.dedicated}</p>
+        <p>Area: ${temple.area}</p>
+        <img loading = "lazy" src="${temple.imageUrl}" alt="The temple of ${templeLocation}" width="500" height="250">
+    </div>
+    `
+    }
+
+    return templeCards
+
+}
+document.querySelector("main h1").textContent = "Home"
+currentDisplay.innerHTML = displayItems(temples);
+
+document.querySelectorAll("nav a").forEach(
+    (link) => link.addEventListener("click",
+        (e) => {
+            e.preventDefault();
+            const oldTemples = temples.filter(temple => parseInt(temple.dedicated.split(",")[0], 10) < 1900);
+            const newTemples = temples.filter(temple => parseInt(temple.dedicated.split(",")[0], 10) > 2000);
+            const largeTemples = temples.filter(temple => temple.area > 90000);
+            const smallTemples = temples.filter(temple => temple.area > 90000);
+
+            const filter = link.textContent.toLowerCase();
+            if (filter === "home") {
+                document.querySelector("main h1").textContent = "Home"
+                currentDisplay.innerHTML = displayItems(temples);
+            }
+
+            else if(filter === "old") {
+                document.querySelector("main h1").textContent = "Old Temples"
+                currentDisplay.innerHTML = displayItems(oldTemples);
+            }
+
+            else if(filter === "new") {
+                document.querySelector("main h1").textContent = "New Temples"
+                currentDisplay.innerHTML = displayItems(newTemples);
+            }
+
+            else if(filter === "large") {
+                document.querySelector("main h1").textContent = "Large Temples"
+                currentDisplay.innerHTML = displayItems(largeTemples);
+            }
+            
+            else {
+                document.querySelector("main h1").textContent = "Small Temples"
+                currentDisplay.innerHTML = displayItems(smallTemples)
+            }
+
+        }
+    )
+)
+
+
+
